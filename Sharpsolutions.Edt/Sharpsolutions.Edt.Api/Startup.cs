346763d8e1dde89118123f145259a0ccc73546f8
jellebens <jellebens@outlook.com>
2014-11-27 17:53:36 +0100

    Register user Api :) Stripped EF crap though

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Api/Startup.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Sharpsolutions.Edt.Api.Startup))]
+
+namespace Sharpsolutions.Edt.Api {
+    public class Startup {
+        public void Configuration(IAppBuilder app) {
+            HttpConfiguration config = new HttpConfiguration();
+            WebApiConfig.Register(config);
+            app.UseWebApi(config);
+        }
+    }
+}
