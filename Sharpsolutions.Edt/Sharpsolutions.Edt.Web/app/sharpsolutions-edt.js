cc83c00d513cd940addea328a27023dcb4b2a558
jellebens <jellebens@outlook.com>
2014-11-27 22:38:06 +0100

    everything is working

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Web/app/sharpsolutions-edt.js
@@ -0,0 +1,40 @@
+﻿var app = angular.module('EdtApp', ['ngRoute', 'LocalStorageModule', 'angular-loading-bar']);
+
+//Configure routing
+var config = function ($routeProvider, $locationProvider) {
+    $locationProvider.hashPrefix('!').html5Mode(true);
+
+    $routeProvider.when("/", {
+        templateUrl: 'app/views/home.html',
+        controller: 'homeController'
+    });
+};
+
+
+//Better like this otherwise might break during minification
+config.$inject = ['$routeProvider', '$locationProvider']
+app.config(config);
+
+app.config(function ($httpProvider) {
+    $httpProvider.interceptors.push('authInterceptorService');
+});
+
+
+
+
+homeController.$inject = ['$scope'];
+app.controller('homeController', homeController);
+
+signupController.$inject = ['$scope', '$location', '$timeout', 'authService'];
+app.controller('signupController', signupController);
+
+authInterceptorService.$inject = ['$q', '$location', 'localStorageService'];
+app.factory('authInterceptorService',  authInterceptorService);
+
+authService.$inject= ['$http', '$q', 'localStorageService']
+app.factory('authService', authService);
+
+
+app.run(['authService', function (authService) {
+    authService.fillAuthData();
+}]);
