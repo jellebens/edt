27ce1af1459959a35bb061896d4cdca74f3e5974
jellebens <jellebens@outlook.com>
2014-11-27 18:18:11 +0100

    Implemented token service yaaay

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Api/Controllers/CommoditiesController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sharpsolutions.Edt.Api.Controllers {
+    [RoutePrefix("commodities")]
+    [Authorize]
+    public class CommoditiesController : ApiController {
+        [Route("")]
+        public IHttpActionResult Commodities() {
+
+            var x = new[] {
+                        new { Category= "Machinery", Name= "Mineral Extractors" },
+                        new { Category= "Metals", Name= "Gallium" }
+                   };
+        
+
+            return Ok(x);
+        }
+
+    }
+}
--- Sharpsolutions.Edt/Sharpsolutions.Edt.Api/Startup.cs
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Api/Startup.cs
@@ -3,15 +3,35 @@ using System.Threading.Tasks;
 using Microsoft.Owin;
 using Owin;
 using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Sharpsolutions.Edt.Api.Security;
 
 [assembly: OwinStartup(typeof(Sharpsolutions.Edt.Api.Startup))]
 
 namespace Sharpsolutions.Edt.Api {
     public class Startup {
         public void Configuration(IAppBuilder app) {
+            ConfigureOAuth(app);
+
             HttpConfiguration config = new HttpConfiguration();
             WebApiConfig.Register(config);
+            app.UseCors(Microsoft.Owin.Cors.CorsOptions.AllowAll);
             app.UseWebApi(config);
+        }
+
+
+        public void ConfigureOAuth(IAppBuilder app) {
+            OAuthAuthorizationServerOptions OAuthServerOptions = new OAuthAuthorizationServerOptions() {
+                AllowInsecureHttp = true,
+                TokenEndpointPath = new PathString("/token"),
+                AccessTokenExpireTimeSpan = TimeSpan.FromDays(1),
+                Provider = new EdtAuthorizationServerProvider()
+            };
+
+            // Token Generation
+            app.UseOAuthAuthorizationServer(OAuthServerOptions);
+            app.UseOAuthBearerAuthentication(new OAuthBearerAuthenticationOptions());
+
         }
     }
 }
