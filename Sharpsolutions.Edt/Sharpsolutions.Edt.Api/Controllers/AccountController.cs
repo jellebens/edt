50beb2d0093a46d19f7cd662f6164e089fe36a9e
jellebens <jellebens@outlook.com>
2014-11-27 18:28:22 +0100

    Added publish profile

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Api/Controllers/AccountController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNet.Identity;
+using Sharpsolutions.Edt.Api.Models.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace Sharpsolutions.Edt.Api.Controllers
+{
+    [RoutePrefix("account")]
+    public class AccountController : ApiController
+    {
+        [AllowAnonymous]
+        [Route("register")]
+
+        public IHttpActionResult Register(CreateModel model) {
+            if (!ModelState.IsValid) {
+                return BadRequest(ModelState);
+            }
+
+            ///TODO register the user :p
+            ///
+
+            return Ok();
+        }
+    }
+}
