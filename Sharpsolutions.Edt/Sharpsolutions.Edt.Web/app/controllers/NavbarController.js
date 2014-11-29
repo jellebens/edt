a345b767c57492b0c055d9cf92d2ef93e91c1b3c
jellebens <jellebens@outlook.com>
2014-11-29 01:14:01 +0100

    MM

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Web/app/controllers/NavbarController.js
@@ -0,0 +1,9 @@
+﻿'use strict';
+var navBarController = function ($scope, $location, authService) {
+    $scope.logOut = function () {
+        authService.logOut();
+        $location.path('/');
+    }
+    
+    $scope.authentication = authService.authentication;
+}
