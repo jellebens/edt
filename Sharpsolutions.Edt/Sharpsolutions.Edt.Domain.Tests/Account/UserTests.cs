db55cb39ccda65b2597aeebaed8411d19489a4f3
jellebens <jellebens@outlook.com>
2014-12-02 22:34:52 +0100

    Data access

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Domain.Tests/Account/UserTests.cs
@@ -0,0 +1,34 @@
+﻿using NUnit.Framework;
+using Sharpsolutions.Edt.Domain.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpsolutions.Edt.Domain.Tests.Account {
+    [TestFixture]
+    public class UserTests {
+        [Test]
+        public void VerifyHavingCorrectPasswordShouldReturnTrue() {
+            string pass = "P@ssw0rd";
+            User u = User.Create("John doe", pass);
+
+            Console.WriteLine(u.PwdHash);
+
+            bool isValid = u.Verify(pass);
+            Assert.IsTrue(isValid);
+        }
+
+        [Test]
+        public void VerifyHavingIncorrectPasswordShouldReturnFalse() {
+            string pass = "P@ssw0rd";
+            User u = User.Create("John doe", pass);
+
+            Console.WriteLine(u.PwdHash);
+
+            bool isValid = u.Verify("HEllo");
+            Assert.IsFalse(isValid);
+        }
+    }
+}
--- Sharpsolutions.Edt/Sharpsolutions.Edt.Domain/Account/User.cs
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Domain/Account/User.cs
@@ -13,15 +13,34 @@ namespace Sharpsolutions.Edt.Domain.Account {
 
         public virtual string PwdHash { get; protected set; }
 
+        public bool Verify(string password) {
+            string hash = CreateHash(password);
+
+            return hash == this.PwdHash;
+        }
 
         public static User Create(string userName, string pwd) {
             User u = new User();
             u.UserName = userName;
             u.Id = userName;
 
-            
+            u.PwdHash = u.CreateHash(pwd);
 
             return u;
+        }
+
+        private string CreateHash(string pwd) {
+            string hash;
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string secret = this.GetType().Name;
+
+            byte[] keyByte = encoding.GetBytes(secret);
+            byte[] messageBytes = encoding.GetBytes(pwd);
+            using (var hmacsha256 = new HMACSHA256(keyByte)) {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                hash = Convert.ToBase64String(hashmessage);
+            }
+            return hash;
         }
 
         
