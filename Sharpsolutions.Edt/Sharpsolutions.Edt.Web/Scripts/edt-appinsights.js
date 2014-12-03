711ad1ce01ce1a974920453b104d02de22c5c8bd
jellebens <jellebens@outlook.com>
2014-12-03 15:57:39 +0100

    Addedappinsights tracking

--- Sharpsolutions.Edt/Sharpsolutions.Edt.Web/Scripts/edt-appinsights.js
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Web/Scripts/edt-appinsights.js
@@ -1,7 +1,7 @@
 ﻿var appInsights = window.appInsights || function (config) {
     function s(config) { t[config] = function () { var i = arguments; t.queue.push(function () { t[config].apply(t, i) }) } } var t = { config: config }, r = document, f = window, e = "script", o = r.createElement(e), i, u; for (o.src = config.url || "//az416426.vo.msecnd.net/scripts/a/ai.0.js", r.getElementsByTagName(e)[0].parentNode.appendChild(o), t.cookie = r.cookie, t.queue = [], i = ["Event", "Exception", "Metric", "PageView", "Trace"]; i.length;) s("track" + i.pop()); return config.disableExceptionTracking || (i = "onerror", s("_" + i), u = f[i], f[i] = function (config, r, f, e, o) { var s = u && u(config, r, f, e, o); return s !== !0 && t["_" + i](config, r, f, e, o), s }), t
 }({
-    instrumentationKey: "efa055d4-5118-4a41-82e6-b7455ee2d298"
+    instrumentationKey: "787e82db-6768-4cb3-9b74-1fc2c37d450d"
 });
 
 window.appInsights = appInsights;
