400b2a8ad4a074afc26f8c9a3641a98459ccd8aa
jellebens <jellebens@outlook.com>
2014-12-03 21:49:50 +0100

    Added missing files

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Domain/Account/Password.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpsolutions.Edt.Domain.Account {
+    public class Password: IEquatable<string> {
+        private const string _val = "CEAB5083-D28A-417E-A49F-C7B01E691119";
+        private string _Hash;
+        private Password() {
+
+        }
+
+        public Password(string hash) {
+            if (string.IsNullOrWhiteSpace(hash)) {
+                throw new ArgumentNullException("Password can not be null or empty");
+            }
+
+            _Hash = hash;
+        }
+
+        public static Password New(string pwd) {
+            if (string.IsNullOrWhiteSpace(pwd)) {
+                throw new ArgumentNullException("Password can not be null or empty");
+            }
+            Password password = new Password();
+            password._Hash = password.Calculate(pwd);
+
+            return password;
+        }
+
+        private string Calculate(string pwd) {
+            string hash;
+            ASCIIEncoding encoding = new ASCIIEncoding();
+            string secret = typeof(User).Name;
+
+            byte[] keyByte = encoding.GetBytes(_val);
+            
+            byte[] messageBytes = encoding.GetBytes(pwd);
+            using (var hmacsha256 = new HMACSHA256(keyByte)) {
+                byte[] hashmessage = hmacsha256.ComputeHash(messageBytes);
+                hash = Convert.ToBase64String(hashmessage);
+            }
+            return hash;
+        }
+
+        public bool Equals(string other) {
+            return string.Equals(_Hash, Calculate(other));
+        }
+
+        public override string ToString() {
+            return _Hash;
+        }
+
+        public override int GetHashCode() {
+            return _Hash.GetHashCode();
+        }
+    }
+}
