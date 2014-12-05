c5ac5e56c114fdff8dea600cb64a9895e7631cb8
jellebens <jellebens@outlook.com>
2014-12-05 21:03:42 +0100

    Added missing files

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Domain.Tests/Commodities/CategoryTests.cs
@@ -0,0 +1,47 @@
+﻿using NUnit.Framework;
+using Sharpsolutions.Edt.Domain.Commodities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpsolutions.Edt.Domain.Tests.Commodities {
+    [TestFixture]
+    public class CategoryTests {
+        private Category _Metals = new Category("Metals");
+        [Test]
+        public void EqualsSameNameShouldEqual() {
+            
+            Category b = new Category("Metals");
+
+            Assert.AreEqual(_Metals, b);
+        }
+
+        [Test]
+        public void EqualsStringSameAsNameShouldBeEqual() {
+            Assert.AreEqual("Metals", _Metals);
+        }
+
+        [Test]
+        public void EqualsStringDifferentNameShouldNotBeEqual() {
+            Assert.AreNotEqual("Medicines", _Metals);
+        }
+
+        [Test]
+        public void EqualsDifferentShouldNotBeEqual() {
+            Category b = new Category("Medicines");
+            Assert.AreNotEqual(b, _Metals);
+        }
+
+        [Test]
+        [ExpectedException(typeof(ArgumentNullException))]
+        public void CtorEmptyStringShouldThrow() {
+            new Category(" ");
+        }
+
+
+
+
+    }
+}
---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Domain/Commodities/Category.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sharpsolutions.Edt.Domain.Commodities {
+    public class Category: IEquatable<string> {
+        public Category(string name) {
+            if (string.IsNullOrWhiteSpace(name)) {
+                throw new ArgumentNullException(name);
+            }
+
+            this.Name = name;
+        }
+        public string Name { get; set; }
+
+        public override string ToString() {
+            return Name;
+        }
+
+        public override bool Equals(object obj) {
+            if(this.GetType() != obj.GetType()){
+                return false;
+            }
+            
+            Category other = obj as Category;
+            if(other == null){
+                return false;
+            }
+
+            return this.Name == other.Name;
+        }
+
+        public override int GetHashCode() {
+            return Name.GetHashCode();
+        }
+
+        public bool Equals(string other) {
+            return this.Name.Equals(other);
+        }
+    }
+}
