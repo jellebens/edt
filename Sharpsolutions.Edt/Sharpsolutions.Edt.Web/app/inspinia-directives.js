0461d2a5b66c1728aab2b29bf60e35916ee2f5dd
jellebens <jellebens@outlook.com>
2014-12-08 21:21:33 +0100

    checkboxes for supply and demand

--- Sharpsolutions.Edt/Sharpsolutions.Edt.Web/app/inspinia-directives.js
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Web/app/inspinia-directives.js
@@ -2,6 +2,27 @@
  * INSPINIA - Responsive Admin Theme
  * Copyright 2014 Webapplayers.com
  *
+ * Main directives.js file
+ * Define directives for used plugin
+ *
+ *
+ * Functions (directives)
+ *  - pageTitle
+ *  - sideNavigation
+ *  - iboxTools
+ *  - minimalizaSidebar
+ *  - vectorMap
+ *  - morrisArea
+ *  - morrisBar
+ *  - morrisLine
+ *  - morrisDonut
+ *  - sparkline
+ *  - icheck
+ *  - ionRangeSlider
+ *  - dropZone
+ *  - fancyBox
+ *  - responsiveVideo
+ *
  */
 
 
@@ -10,19 +31,19 @@
  */
 function pageTitle($rootScope, $timeout) {
     return {
-        link: function(scope, element) {
-            var listener = function(event, toState, toParams, fromState, fromParams) {
+        link: function (scope, element) {
+            var listener = function (event, toState, toParams, fromState, fromParams) {
                 // Default title - load on Dashboard 1
                 var title = 'INSPINIA | Responsive Admin Theme';
                 // Create your own title pattern
                 if (toState.data && toState.data.pageTitle) title = 'INSPINIA | ' + toState.data.pageTitle;
-                $timeout(function() {
+                $timeout(function () {
                     element.text(title);
                 });
             };
             $rootScope.$on('$stateChangeStart', listener);
         }
-    }
+    };
 };
 
 /**
@@ -31,12 +52,35 @@ function pageTitle($rootScope, $timeout) {
 function sideNavigation() {
     return {
         restrict: 'A',
-        link: function(scope, element) {
+        link: function (scope, element) {
             // Call the metsiMenu plugin and plug it to sidebar navigation
             element.metisMenu();
         }
     };
 };
+
+/**
+ * responsibleVideo - Directive for responsive video
+ */
+function responsiveVideo() {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            var figure = element;
+            var video = element.children();
+            video
+                .attr('data-aspectRatio', video.height() / video.width())
+                .removeAttr('height')
+                .removeAttr('width'); //We can use $watch on $window.innerWidth also.
+            $(window).resize(function () {
+                var newWidth = figure.width();
+                video
+                    .width(newWidth)
+                    .height(newWidth * video.attr('data-aspectRatio'));
+            }).resize();
+        }
+    };
+}
 
 /**
  * iboxTools - Directive for iBox tools elements in right corner of ibox
@@ -45,7 +89,7 @@ function iboxTools($timeout) {
     return {
         restrict: 'A',
         scope: true,
-        templateUrl: 'app/views/common/ibox_tools.html',
+        templateUrl: 'views/common/ibox_tools.html',
         controller: function ($scope, $element) {
             // Function for collapse ibox
             $scope.showhide = function () {
@@ -61,11 +105,11 @@ function iboxTools($timeout) {
                     ibox.find('[id^=map-]').resize();
                 }, 50);
             },
-                // Function for close ibox
+            // Function for close ibox
                 $scope.closebox = function () {
                     var ibox = $element.closest('div.ibox');
                     ibox.remove();
-                }
+                };
         }
     };
 };
@@ -86,15 +130,242 @@ function minimalizaSidebar($timeout) {
                     // For smoothly turn on menu
                     $timeout(function () {
                         $('#side-menu').fadeIn(500);
-                        }, 100);
+                    }, 100);
                 } else {
                     // Remove all inline style from jquery fadeIn function to reset menu state
                     $('#side-menu').removeAttr('style');
                 }
-            }
+            };
         }
     };
 };
+
+/**
+ * vectorMap - Directive for Vector map plugin
+ */
+function vectorMap() {
+    return {
+        restrict: 'A',
+        scope: {
+            myMapData: '=',
+        },
+        link: function (scope, element, attrs) {
+            element.vectorMap({
+                map: 'world_mill_en',
+                backgroundColor: "transparent",
+                regionStyle: {
+                    initial: {
+                        fill: '#e4e4e4',
+                        "fill-opacity": 0.9,
+                        stroke: 'none',
+                        "stroke-width": 0,
+                        "stroke-opacity": 0
+                    }
+                },
+                series: {
+                    regions: [
+                        {
+                            values: scope.myMapData,
+                            scale: ["#1ab394", "#22d6b1"],
+                            normalizeFunction: 'polynomial'
+                        }
+                    ]
+                },
+            });
+        }
+    };
+}
+
+
+/**
+ * morrisArea - Directive for Morris chart - type Area
+ */
+function morrisArea() {
+    return {
+        restrict: 'A',
+        scope: {
+            chartOptions: '='
+        },
+        link: function (scope, element, attrs) {
+            var chartDetail = scope.chartOptions;
+            chartDetail.element = attrs.id;
+            var chart = new Morris.Area(chartDetail);
+            return chart;
+        }
+    };
+}
+
+/**
+ * morrisBar - Directive for Morris chart - type Bar
+ */
+function morrisBar() {
+    return {
+        restrict: 'A',
+        scope: {
+            chartOptions: '='
+        },
+        link: function (scope, element, attrs) {
+            var chartDetail = scope.chartOptions;
+            chartDetail.element = attrs.id;
+            var chart = new Morris.Bar(chartDetail);
+            return chart;
+        }
+    };
+}
+
+/**
+ * morrisLine - Directive for Morris chart - type Line
+ */
+function morrisLine() {
+    return {
+        restrict: 'A',
+        scope: {
+            chartOptions: '='
+        },
+        link: function (scope, element, attrs) {
+            var chartDetail = scope.chartOptions;
+            chartDetail.element = attrs.id;
+            var chart = new Morris.Line(chartDetail);
+            return chart;
+        }
+    };
+}
+
+/**
+ * morrisDonut - Directive for Morris chart - type Donut
+ */
+function morrisDonut() {
+    return {
+        restrict: 'A',
+        scope: {
+            chartOptions: '='
+        },
+        link: function (scope, element, attrs) {
+            var chartDetail = scope.chartOptions;
+            chartDetail.element = attrs.id;
+            var chart = new Morris.Donut(chartDetail);
+            return chart;
+        }
+    };
+}
+
+/**
+ * sparkline - Directive for Sparkline chart
+ */
+function sparkline() {
+    return {
+        restrict: 'A',
+        scope: {
+            sparkData: '=',
+            sparkOptions: '=',
+        },
+        link: function (scope, element, attrs) {
+            scope.$watch(scope.sparkData, function () {
+                render();
+            });
+            scope.$watch(scope.sparkOptions, function () {
+                render();
+            });
+            var render = function () {
+                $(element).sparkline(scope.sparkData, scope.sparkOptions);
+            };
+        }
+    };
+};
+
+/**
+ * icheck - Directive for custom checkbox icheck
+ */
+function icheck($timeout) {
+    return {
+        restrict: 'A',
+        require: 'ngModel',
+        link: function ($scope, element, $attrs, ngModel) {
+            return $timeout(function () {
+                var value;
+                value = $attrs['value'];
+
+                $scope.$watch($attrs['ngModel'], function (newValue) {
+                    $(element).iCheck('update');
+                });
+                return $(element).iCheck({
+                    checkboxClass: 'icheckbox_square-green',
+                    radioClass: 'iradio_square-green'
+
+                }).on('ifChanged', function (event) {
+                    if ($(element).attr('type') === 'checkbox' && $attrs['ngModel']) {
+                        $scope.$apply(function () {
+                            return ngModel.$setViewValue(event.target.checked);
+                        });
+                    }
+                    if ($(element).attr('type') === 'radio' && $attrs['ngModel']) {
+                        return $scope.$apply(function () {
+                            return ngModel.$setViewValue(value);
+                        });
+                    }
+                });
+            });
+        }
+    };
+}
+
+/**
+ * ionRangeSlider - Directive for Ion Range Slider
+ */
+function ionRangeSlider() {
+    return {
+        restrict: 'A',
+        scope: {
+            rangeOptions: '='
+        },
+        link: function (scope, elem, attrs) {
+            elem.ionRangeSlider(scope.rangeOptions);
+        }
+    };
+}
+
+/**
+ * dropZone - Directive for Drag and drop zone file upload plugin
+ */
+function dropZone() {
+    return function (scope, element, attrs) {
+        element.dropzone({
+            url: "/upload",
+            maxFilesize: 100,
+            paramName: "uploadfile",
+            maxThumbnailFilesize: 5,
+            init: function () {
+                scope.files.push({ file: 'added' });
+                this.on('success', function (file, json) {
+                });
+                this.on('addedfile', function (file) {
+                    scope.$apply(function () {
+                        alert(file);
+                        scope.files.push({ file: 'added' });
+                    });
+                });
+                this.on('drop', function (file) {
+                    alert('file');
+                });
+            }
+        });
+    };
+}
+
+/**
+ * fancyBox - Directive for Fancy Box plugin used in simple gallery view
+ */
+function fancyBox() {
+    return {
+        restrict: 'A',
+        link: function (scope, element) {
+            element.fancybox({
+                openEffect: 'none',
+                closeEffect: 'none'
+            });
+        }
+    };
+}
 
 /**
  *
@@ -106,3 +377,14 @@ angular
     .directive('sideNavigation', sideNavigation)
     .directive('iboxTools', iboxTools)
     .directive('minimalizaSidebar', minimalizaSidebar)
+    //.directive('vectorMap', vectorMap)
+    //.directive('morrisArea', morrisArea)
+    //.directive('morrisBar', morrisBar)
+    //.directive('morrisLine', morrisLine)
+    //.directive('morrisDonut', morrisDonut)
+    //.directive('sparkline', sparkline)
+    .directive('icheck', icheck);
+    //.directive('ionRangeSlider', ionRangeSlider)
+    //.directive('dropZone', dropZone)
+    //.directive('fancyBox', fancyBox)
+    //.directive('responsiveVideo', responsiveVideo)
