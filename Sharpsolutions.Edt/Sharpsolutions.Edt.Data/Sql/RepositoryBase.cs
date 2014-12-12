9030a65ac0cbc845c2b2b46d60ad585c927a163d
jellebens <jellebens@outlook.com>
2014-12-12 21:29:33 +0100

    added missing files

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Data/Sql/RepositoryBase.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Data.Entity.Infrastructure;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Castle.Core.Logging;
+using Sharpsolutions.Edt.System.Data;
+using Sharpsolutions.Edt.System.Domain;
+using Sharpsolutions.Edt.System.Logging;
+
+namespace Sharpsolutions.Edt.Data.Sql {
+    public abstract class RepositoryBase<TEntity>: IRepository<TEntity>, IDisposable where TEntity : IEntity
+    {
+        protected readonly DbContext DbContext;
+        private readonly ILogger _Logger;
+        private readonly bool _OwnsContext = false;
+
+        protected RepositoryBase(DbContext dbContext, ILoggerFactory loggerFactory) {
+            DbContext = dbContext;
+            _Logger = loggerFactory.Create(Loggers.System.Sql);
+
+            DbContext.Database.Log = (s) => _Logger.DebugFormat("{0}", s.Replace(Environment.NewLine, string.Empty));
+        }
+
+        protected RepositoryBase(ILoggerFactory loggerFactory): this(new EdtDbContext(), loggerFactory)
+        {
+            _OwnsContext = true;
+        }
+
+
+        public abstract void Add(TEntity entity);
+
+        public abstract TEntity Get(Guid id);
+
+        public abstract IQueryable<TEntity> Query();
+
+        public void Commit()
+        {
+            DbContext.SaveChanges();
+        }
+
+        public void Dispose() {
+            if (_OwnsContext)
+            {
+                DbContext.Dispose();
+            }
+        }
+    }
+}
---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Data/Sql/StarportRepository.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data.Entity;
+using System.Linq;
+using Castle.Core.Logging;
+using Sharpsolutions.Edt.Domain.Trade;
+using Sharpsolutions.Edt.System.Data;
+
+namespace Sharpsolutions.Edt.Data.Sql {
+    public class StarportRepository: RepositoryBase<Starport>
+    {
+        public StarportRepository(DbContext dbContext, ILoggerFactory loggerFactory) : base(dbContext, loggerFactory)
+        {
+        }
+
+        public override void Add(Starport entity)
+        {
+            DbContext.Set<Starport>().Add(entity);
+        }
+
+
+        public override Starport Get(Guid id)
+        {
+            return DbContext.Set<Starport>().Find(id);
+        }
+
+        public override IQueryable<Starport> Query()
+        {
+            return DbContext.Set<Starport>().AsQueryable();
+        }
+    }
+}
