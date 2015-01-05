12e2f8025da2aecab92276edbff0e868b7eb835a
jellebens <jellebens@outlook.com>
2015-01-05 16:30:04 +0100

    Added protractor specs

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Web/app/tests/spec.js
@@ -0,0 +1,8 @@
+﻿// spec.js
+describe('angularjs homepage', function () {
+    it('should have a title', function () {
+        browser.get('https://localhost:44300/edt/app');
+
+        expect(browser.getTitle()).toEqual('Elite: Dangerous - Trading App');
+    });
+});
