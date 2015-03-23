e8497b44324b7562860650276ff338b1f086c00f
jellebens <jellebens@outlook.com>
2015-03-23 21:08:25 +0100

    Added timestampt to usercreated

--- Sharpsolutions.Edt/Sharpsolutions.Edt.Domain/Account/Events/UserCreated.cs
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Domain/Account/Events/UserCreated.cs
@@ -9,11 +9,12 @@ namespace Sharpsolutions.Edt.Domain.Account.Events {
     public class UserCreated : EventBase {
         public string Username { get; private set; }
         public Password Password { get; private set; }
-
+        public DateTime Timestamp { get; set; }
 
         public UserCreated(string username, Password password) {
             Username = username;
             Password = password;
+            Timestamp = DateTime.UtcNow;
         }
     }
 }
