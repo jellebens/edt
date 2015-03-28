f5ab7cea300872534b3bc71f8c528a2801cbc528
jellebens <jellebens@outlook.com>
2015-03-28 18:57:18 +0100

    Scripted setup of local dev environment Changed API and Web frontend to their respective sites so i can script the webservers in the webtest

---
+++ Sharpsolutions.Edt/.deployment/local/create-dev-env.ps1
@@ -0,0 +1,77 @@
+﻿function New-Development-Site {
+           
+  <#
+  .SYNOPSIS
+  Creates a new website in IIS that is accessible onl by ssl
+  .DESCRIPTION
+  Creates a new website in IIS that is accessible onl by ssl
+  .EXAMPLE
+  Give an example of how to use it
+  .EXAMPLE
+  Give another example of how to use it
+  .PARAMETER computername
+  The computer name to query. Just one.
+  .PARAMETER logname
+  The name of a file to write failed computer names to. Defaults to errors.txt.
+  #>
+   param ($iisPort, $directoryPath)
+   
+   Import-Module WebAdministration
+
+    $iisAppPoolName = "localhost$iisPort"
+    $iisAppPoolDotNetVersion = "v4.0"
+    $iisAppName = "localhost$iisPort"
+
+    #navigate to the app pools root
+    cd IIS:\AppPools\
+
+    #create the directory
+    if(!(Test-Path $directoryPath)){
+    New-Item $directoryPath -ItemType directory
+}
+
+    #check if the app pool exists
+    if ((Test-Path $iisAppPoolName -pathType container))
+    {
+    Remove-Item $iisAppPoolName
+}
+
+#create the app pool
+$appPool = New-Item $iisAppPoolName
+$appPool | Set-ItemProperty -Name "managedRuntimeVersion" -Value $iisAppPoolDotNetVersion
+
+#navigate to the sites root
+cd IIS:\Sites\
+
+#check if the site exists
+if (Test-Path $iisAppName -pathType container)
+{
+    Remove-Item $iisAppName
+}
+
+#create the site
+$iisApp = New-Item $iisAppName -bindings @{protocol="https";bindingInformation=":"+ $iisPort +":localhost"} -physicalPath $directoryPath
+$iisApp | Set-ItemProperty -Name "applicationPool" -Value $iisAppPoolName
+
+$certifcate = (Get-ChildItem -Path Cert:\LocalMachine\My | Where-Object {$_.Subject -match "CN=localhost"});
+
+$thumbprint = $certifcate.Thumbprint.ToString();
+
+
+$sslBindingPath = "IIS:\SslBindings\0.0.0.0!$iisPort"
+
+if(Test-Path $sslBindingPath){
+    Remove-Item $sslBindingPath
+}
+
+new-item -Path $sslBindingPath  -Value $certifcate
+
+              
+
+}
+
+
+
+
+New-Development-Site -iisPort 44300 -directoryPath "$PsScriptRoot\..\..\Sharpsolutions.Edt.Web"
+New-Development-Site -iisPort 44301 -directoryPath "$PsScriptRoot\..\..\Sharpsolutions.Edt.Api"
---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.System.WebTest/ExtractionRules/ExtractBearerToken.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Diagnostics;
+using System.Linq;
+using System.Net;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualStudio.TestTools.WebTesting;
+using Newtonsoft.Json;
+
+namespace Sharpsolutions.Edt.System.WebTest.ExtractionRules {
+    
+    [DisplayName("Extract Bearer Token")]
+    public class ExtractBearerToken : ExtractionRule {
+        private class Respone {
+            public string access_token { get; set; }
+            public string token_type { get; set; }
+            public int expires_in { get; set; }
+        }
+
+        public override void Extract(object sender, ExtractionEventArgs e) {
+            if (e.Response.BodyString != null)
+            {
+                if (e.Response.StatusCode == HttpStatusCode.OK)
+                {
+                    var json = e.Response.BodyString;
+                    Respone r = JsonConvert.DeserializeObject<Respone>(json);
+                    e.WebTest.Context.Add(this.ContextParameterName, r.access_token);
+                    e.Message = string.Format("Extracted Bearertoken into: {0}",this.ContextParameterName);
+                    e.Success = true;
+                }
+                else
+                {
+                    e.Message = string.Format("Authentication failed: {0} ", e.Response.BodyString);
+                    e.Success = false;
+                }
+
+            }
+            else
+            {
+                e.Message = "No response or response empty";
+                e.Success = false;
+            }
+            
+        }
+    }
+}
