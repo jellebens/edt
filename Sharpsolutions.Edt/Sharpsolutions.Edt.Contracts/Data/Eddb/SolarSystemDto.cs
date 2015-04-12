5511829da7462f9e6639f2cca6526694f757b8e4
jellebens <jellebens@outlook.com>
2015-04-12 17:51:58 +0200

    added missing files

---
+++ Sharpsolutions.Edt/Sharpsolutions.Edt.Contracts/Data/Eddb/SolarSystemDto.cs
@@ -0,0 +1,23 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace Sharpsolutions.Edt.Contracts.Data.Eddb
+{
+    [SuppressMessage("ReSharper", "InconsistentNaming")]
+    public class SolarSystemDto
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public double? x { get; set; }
+        public double? y { get; set; }
+        public double? z { get; set; }
+        public string faction { get; set; }
+        public long? population { get; set; }
+        public string government { get; set; }
+        public string allegiance { get; set; }
+        public string state { get; set; }
+        public string security { get; set; }
+        public string primary_economy { get; set; }
+        public int? needs_permit { get; set; }
+        public int updated_at { get; set; }
+    }
+}
